a985df37493ab0b3c4b92c91f95badb1772c9747
Heydar <h_h_z_n@yahoo.com>
2024-01-17 12:07:17 +0330

    choose branch modal after script

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,13 +1,29 @@
 const headerBtn=document.querySelector('.header__btn')
+const chooseBranchModal=document.querySelector('.choose__branchModal')
+const closeChooseBranchBtn=document.querySelector('.close__chooseBranch--btn')
 const topicBtnArray=document.querySelectorAll('.topic__btn')
+function closeChooseBranchModal(){
+    overlay.classList.remove('overlay__active')
+    chooseBranchModal.classList.remove('choose__branchModal--active')
+}
 function openChooseBranchModal(){
-    console.log('first')
+    overlay.classList.add('overlay__active')
+    chooseBranchModal.classList.add('choose__branchModal--active')
 }
 headerBtn.addEventListener('click',()=>{
     openChooseBranchModal()
+})
+closeChooseBranchBtn.addEventListener('click',()=>{
+    closeChooseBranchModal()
 })
 topicBtnArray.forEach(item=>{
     item.addEventListener('click',()=>{
         openChooseBranchModal()
     })
+})
+overlay.addEventListener('click',(e)=>{
+    e.preventDefault()
+    if(e.target.parentElement.tagName=='BODY'){
+        closeChooseBranchModal()
+    }
 })

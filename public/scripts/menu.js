ebe0653ed23ee5a44c228548d1edc781b0f0f993
Heydar <h_h_z_n@yahoo.com>
2024-01-20 18:45:45 +0330

    topic after script

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,5 +1,6 @@
 const galleryModal=document.querySelector('.gallery__modal')
 const closeGallery=document.querySelector('.close__gallery')
+const topicItemArray=document.querySelectorAll('.topic__item')
 function closeGalleryModal(){
     overlay.classList.remove('overlay__active')
     galleryModal.classList.remove('gallery__modal--active')
@@ -16,4 +17,12 @@ overlay.addEventListener('click',(e)=>{
     if(e.target.parentElement.tagName=='BODY'){
         closeGalleryModal()
     }
+})
+topicItemArray.forEach(item=>{
+    item.addEventListener('click',(e)=>{
+        topicItemArray.forEach(item=>{
+            item.classList.remove('topic__item--active')
+        })
+        e.target.classList.add('topic__item--active')
+    })
 })

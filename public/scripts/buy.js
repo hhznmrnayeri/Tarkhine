e114e20c95eb7ee24be694afc7915727a91f10be
Heydar <h_h_z_n@yahoo.com>
2024-01-23 00:52:37 +0330

    section wrapper after script

--- public/scripts/buy.js
+++ public/scripts/buy.js
@@ -1,10 +1,14 @@
 const stateItemArray=document.querySelectorAll('.state__item')
+const sectionItemArray=document.querySelectorAll('.section__item')
 stateItemArray.forEach(item=>{
     item.addEventListener('click',()=>{
         stateItemArray.forEach(state=>{
             state.classList.remove('state__item--active')
         })
+        sectionItemArray.forEach(section=>{
+            section.classList.remove('section__item--active')
+        })
         item.classList.add('state__item--active')
-        console.log(item.dataset.id)
+        document.getElementById(item.dataset.id).classList.add('section__item--active')
     })
 })

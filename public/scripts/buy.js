929fdf6c8cedb8f49e99ffc9ef01f1750458a8c5
Heydar <h_h_z_n@yahoo.com>
2024-01-27 00:58:02 +0330

    delete all modal after style

--- public/scripts/buy.js
+++ public/scripts/buy.js
@@ -1,5 +1,8 @@
 const stateItemArray=document.querySelectorAll('.state__item')
 const sectionItemArray=document.querySelectorAll('.section__item')
+const deleteAllBasketBtn=document.querySelector('.deleteAll__basket')
+const basketWrapper=document.querySelector('.basket__wrapper')
+const emptyBasket=document.querySelector('.empty__basket')
 stateItemArray.forEach(item=>{
     item.addEventListener('click',()=>{
         stateItemArray.forEach(state=>{

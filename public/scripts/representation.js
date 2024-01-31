a3eefbc0ee687896e0ef16240dc62db6bc8e8d51
Heydar <h_h_z_n@yahoo.com>
2024-01-31 10:01:40 +0330

    calendar after script

--- public/scripts/representation.js
+++ public/scripts/representation.js
@@ -1,7 +1,27 @@
 const shireWrapper=document.querySelector('.shire__wrapper')
 const shireBox=document.querySelector('.shire__box')
+const calendarWrapper=document.querySelector('.calendar__wrapper')
+const calendarBox=document.querySelector('.calendar__box')
+const dayCalendarArray=document.querySelectorAll('.day__calendar')
+dayCalendarArray.forEach(item=>{
+    item.addEventListener('click',(e)=>{
+        e.preventDefault()
+        if(item.classList.contains('day__disable')){
+            return
+        }else{
+            dayCalendarArray.forEach(day=>{
+                day.classList.remove('day__active')
+            })
+        }
+        item.classList.add('day__active')
+    })
+})
 shireWrapper.addEventListener('click',()=>{
     shireBox.classList.add('shire__box--active')
+    overlay.classList.add('overlay__subMenu--active')
+})
+calendarWrapper.addEventListener('click',()=>{
+    calendarBox.classList.add('calendar__box--active')
     overlay.classList.add('overlay__subMenu--active')
 })
 overlay.addEventListener('click',(e)=>{
@@ -9,5 +29,6 @@ overlay.addEventListener('click',(e)=>{
     if(e.target.parentElement.tagName=='BODY'){
         overlay.classList.remove('overlay__subMenu--active')
         shireBox.classList.remove('shire__box--active')
+        calendarBox.classList.remove('calendar__box--active')
     }
 })

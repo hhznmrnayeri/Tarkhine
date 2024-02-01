0f00fbf084966c8109d3f9278808d102560452dd
Heydar <h_h_z_n@yahoo.com>
2024-02-02 01:42:35 +0330

    section wrapper after script

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,10 +1,14 @@
 const profileStateArray=document.querySelectorAll('.profile__state')
+const profileSectionArray=document.querySelectorAll('.profile__section')
 profileStateArray.forEach(state=>{
     state.addEventListener('click',()=>{
         profileStateArray.forEach(item=>{
             item.classList.remove('profile__state--active')
         })
+        profileSectionArray.forEach(item=>{
+            item.classList.remove('profile__section--active')
+        })
     state.classList.add('profile__state--active')
-    console.log(state.dataset.id)
+    document.getElementById(state.dataset.id).classList.add('profile__section--active')
     })
 })

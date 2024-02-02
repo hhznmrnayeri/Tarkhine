6194170aa391af8dc77096e3176a846c455e688d
Heydar <h_h_z_n@yahoo.com>
2024-02-02 05:36:04 +0330

    calendar after script

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -5,6 +5,9 @@ const editBox=document.querySelector('.edit__box')
 const editProfile=document.querySelector('.edit__profile')
 const cancelBtn=document.querySelector('.cancel__btn')
 const submitBtn=document.querySelector('.submit__btn')
+const calendarWrapper=document.querySelector('.calendar__wrapper')
+const calendarBox=document.querySelector('.calendar__box')
+const dayCalendarArray=document.querySelectorAll('.day__calendar')
 profileStateArray.forEach(state=>{
     state.addEventListener('click',()=>{
         profileStateArray.forEach(item=>{
@@ -30,4 +33,28 @@ cancelBtn.addEventListener('click',(e)=>{
 e.preventDefault()
     profileBox.classList.add('profile__box--active')
     editBox.classList.remove('edit__box--active')
+})
+dayCalendarArray.forEach(item=>{
+    item.addEventListener('click',(e)=>{
+        e.preventDefault()
+        if(item.classList.contains('day__disable')){
+            return
+        }else{
+            dayCalendarArray.forEach(day=>{
+                day.classList.remove('day__active')
+            })
+        }
+        item.classList.add('day__active')
+    })
+})
+calendarWrapper.addEventListener('click',()=>{
+    calendarBox.classList.add('calendar__box--active')
+    overlay.classList.add('overlay__subMenu--active')
+})
+overlay.addEventListener('click',(e)=>{
+    e.preventDefault()
+    if(e.target.parentElement.tagName=='BODY'){
+        overlay.classList.remove('overlay__subMenu--active')
+        calendarBox.classList.remove('calendar__box--active')
+    }
 })

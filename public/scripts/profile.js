03ee9cc2cbb4aaa6ba6c7933ac828eebd1e68cc5
Heydar <h_h_z_n@yahoo.com>
2024-02-02 05:04:59 +0330

    profile user after script

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,5 +1,10 @@
 const profileStateArray=document.querySelectorAll('.profile__state')
 const profileSectionArray=document.querySelectorAll('.profile__section')
+const profileBox=document.querySelector('.profile__box')
+const editBox=document.querySelector('.edit__box')
+const editProfile=document.querySelector('.edit__profile')
+const cancelBtn=document.querySelector('.cancel__btn')
+const submitBtn=document.querySelector('.submit__btn')
 profileStateArray.forEach(state=>{
     state.addEventListener('click',()=>{
         profileStateArray.forEach(item=>{
@@ -11,4 +16,18 @@ profileStateArray.forEach(state=>{
     state.classList.add('profile__state--active')
     document.getElementById(state.dataset.id).classList.add('profile__section--active')
     })
+})
+editProfile.addEventListener('click',()=>{
+    profileBox.classList.remove('profile__box--active')
+    editBox.classList.add('edit__box--active')
+})
+submitBtn.addEventListener('click',(e)=>{
+e.preventDefault()
+    profileBox.classList.add('profile__box--active')
+    editBox.classList.remove('edit__box--active')
+})
+cancelBtn.addEventListener('click',(e)=>{
+e.preventDefault()
+    profileBox.classList.add('profile__box--active')
+    editBox.classList.remove('edit__box--active')
 })

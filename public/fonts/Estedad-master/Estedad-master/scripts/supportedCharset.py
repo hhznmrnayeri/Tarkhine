12631482d40d7b682734e5a16ab1c2f13d9707ae
Heydar <h_h_z_n@yahoo.com>
2024-02-15 20:56:56 +0330

    refactor font folder

--- public/fonts/Estedad-master/Estedad-master/scripts/supportedCharset.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from fontTools.ttLib import TTFont
-
-font_path = os.path.abspath(os.getcwd()) + "\\" +sys.argv[1]
-f = TTFont(font_path)
-cmap = f.getBestCmap()
-for char in sorted(cmap):
-    print(chr(char), end =" ")

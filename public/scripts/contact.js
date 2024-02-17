7295303d3c24d52cf0359d537e707d42136a1a54
Heydar <h_h_z_n@yahoo.com>
2024-02-17 20:15:10 +0330

    refactor contact page

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -1,6 +1,7 @@
 const galleryBranch=document.querySelector('.gallery__branch')
 const closeGallery=document.querySelector('.close__gallery')
 const galleryIconArray=document.querySelectorAll('.gallery__icon')
+const branchBtnArray=document.querySelectorAll('.branch__btn')
 function closeGalleryBranch(){
     overlay.classList.remove('overlay__active')
     galleryBranch.classList.remove('gallery__branch--active')
@@ -15,6 +16,11 @@ closeGallery.addEventListener('click',()=>{
 galleryIconArray.forEach(item=>{
     item.addEventListener('click',()=>{
         openGalleryBranch()
+    })
+})
+branchBtnArray.forEach(item=>{
+    item.addEventListener('click',()=>{
+        window.location.href='branch.html'
     })
 })
 overlay.addEventListener('click',(e)=>{

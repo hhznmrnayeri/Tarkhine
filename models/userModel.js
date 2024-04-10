1432f07a70f3eef89c151206e17f5f8d1b9a641e
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-10 12:20:57 +0330

    create userModel

---
+++ models/userModel.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+  },
+  userName: {
+    type: String,
+    required: [true, "A user most have username"],
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "A user most have email address"],
+    unique: true,
+    trim: true,
+  },
+  phoneNumber: {
+    type: Number,
+    trim: true,
+  },
+  dateOfBirth: {
+    type: Number,
+    trim: true,
+  },
+  orders: {
+    type: String,
+  },
+  locations: {
+    type: String,
+  },
+  favorites: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default : Date.now,
+  },
+});

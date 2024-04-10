842b7d7d343072e9df9188ac5942a1fbefa538bb
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-10 12:33:12 +0330

    create router and controllers

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const User = require("./../models/userModel");
+
+exports.getAllUser = () => {};
+
+exports.getUser = () => {};
+
+exports.createUser = () => {};
+
+exports.updateUser = () => {};
+
+exports.deleteUser = () => {}
---
+++ routes/userRouter.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const userController = require("./../controllers/userController");
+
+router
+  .route("/")
+  .get(userController.getAllUser)
+  .post(userController.createUser);
+
+router
+  .route("/:id")
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);

b6bc7f5fdd9199c199b71106285384e54e7632d7
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-12 14:17:03 +0330

    finish getAllUser function

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,29 +1,46 @@
-"use strict"
-const Food = require('./../models/foodModel');
+"use strict";
+const Food = require("./../models/foodModel");
 
-exports.getAllFood = async (req , res) => {};
+exports.getAllFood = async (req, res) => {
+  try {
+    const foods = await Food.find();
+  
+    res.status(200).json({
+        status : 'success',
+        result : foods.length,
+        data : {
+            foods : foods
+        }
+    })
 
-exports.getFood = async (req , res) => {};
-
-exports.createFood = async (req , res) => {
-    const newFood = await Food.create(req.body)
-
-    try {
-        res.status(201).json({
-            status : 'success',
-            data : {food : newFood}
-        })
-    } catch (err) {
-        res.status(400).json({
-            status : 'fail',
-            data : err
-        })
-    }
-
+} catch (err) {
+    res.status(400).json({
+        status : 'fail' ,
+        data : err
+    })
+}
 };
 
-exports.updateFood = async (req , res) => {};
+exports.getFood = async (req, res) => {};
 
-exports.deleteFood = async (req , res) => {};
+exports.createFood = async (req, res) => {
+  const newFood = await Food.create(req.body);
 
-exports.getFilteredFood = async (req , res) => {};
+  try {
+    res.status(201).json({
+      status: "success",
+      data: { food: newFood },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      data: err,
+    });
+  }
+};
+
+exports.updateFood = async (req, res) => {};
+
+exports.deleteFood = async (req, res) => {};
+
+exports.getFilteredFood = async (req, res) => {};

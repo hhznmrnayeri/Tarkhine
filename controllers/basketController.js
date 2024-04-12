649b20cf2c8ac1d41dfb383827ee56d15a7dd7d1
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-12 20:32:42 +0330

    create basketController

---
+++ controllers/basketController.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const Basket = require('./../models/basketModel')
+
+exports.getAllItems = async (req ,res) => {}
+
+exports.addItem = async (req ,res) => {}
+
+exports.updateItem = async (req ,res) => {}
+
+exports.deleteItem = async (req ,res) => {}

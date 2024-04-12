4bf16aa63f35abf4a5f1cdd0653b7166a6ba6b7a
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-12 14:21:17 +0330

    finish getUser function

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -4,24 +4,34 @@ const Food = require("./../models/foodModel");
 exports.getAllFood = async (req, res) => {
   try {
     const foods = await Food.find();
-  
-    res.status(200).json({
-        status : 'success',
-        result : foods.length,
-        data : {
-            foods : foods
-        }
-    })
 
-} catch (err) {
+    res.status(200).json({
+      status: "success",
+      result: foods.length,
+      data: {
+        foods: foods,
+      },
+    });
+  } catch (err) {
     res.status(400).json({
-        status : 'fail' ,
-        data : err
-    })
-}
+      status: "fail",
+      data: err,
+    });
+  }
 };
 
-exports.getFood = async (req, res) => {};
+exports.getFood = async (req, res) => {
+  try {
+    const food = await Food.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        food: food,
+      },
+    });
+  } catch (err) {}
+};
 
 exports.createFood = async (req, res) => {
   const newFood = await Food.create(req.body);

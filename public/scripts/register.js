ef56328001ba2fa226aeacd84934c4fe83420d4e
Heydar <h_h_z_n@yahoo.com>
2024-05-14 22:46:01 +0330

    delete node module

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -7,6 +7,6 @@ registerBtn.addEventListener("click", (event) => {
   register();
 });
 window.addEventListener('load',()=>{
-  fetch(`http://localhost:3000/app/users`)
+  fetch(`http://127.0.0.1:3000/app/users`)
   .then(res=>console.log(res))
 })

92ad1902fcabc88e90fa96d0aff5e1a3fb77c4b2
Heydar <h_h_z_n@yahoo.com>
2024-06-07 11:08:55 +0330

    create SearchBox component and import to Index.jsx

--- React/src/components/Index/Index.jsx
+++ React/src/components/Index/Index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Nav from '../share/Nav'
 import Footer from '../share/Footer'
 import Header from '../share/Header'
+import SearchBox from '../share/SearchBox'
 export default function Index() {
   return (
     <div>
       <Nav title='home'/>
       <Header title='تجربه غذای سالم گیاهی به سبک ترخینه'/>
+      <SearchBox/>
       <Footer/>
     </div>
   )
---
+++ React/src/components/share/SearchBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { RiSearchLine } from "react-icons/ri";
+export default function SearchBox() {
+return (
+    <div className='container'>
+        {/* search bar */}
+        <form className="mt-4 flex items-center justify-between px-4 py-2 border border-gray-400 rounded md:hidden ">
+            {/* search input */}
+            <input type="text" className="border-none outline-none text-2xs flex-grow" placeholder="جستجو"/>
+            {/* search btn */}
+            <button className="mobile__search--btn">
+                <RiSearchLine className="w-4 h-4"/>
+            </button>
+        </form>
+    </div>
+)
+}

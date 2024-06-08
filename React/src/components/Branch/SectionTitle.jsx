6c242f534abc588d0e987c21efca5559679642d2
Heydar <h_h_z_n@yahoo.com>
2024-06-09 03:27:29 +0330

    refactor SectionTitle component

--- React/src/components/Branch/SectionTitle.jsx
+++ React/src/components/Branch/SectionTitle.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 export default function SectionTitle(props) {
 return (
-    <h2 className={`font-estedadBold md:text-2xl ${props.color}`}>{props.title}</h2>
+    <h2 className={`font-estedadBold md:text-2xl ${props.center?'mx-auto text-center':''} ${props.color}`}>{props.title}</h2>
 )
 }

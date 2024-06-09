dca0a674ddca80a099400fbbbb56a27c42c72d95
Heydar <h_h_z_n@yahoo.com>
2024-06-09 04:55:23 +0330

    import Header component to Menu.jsx

--- React/src/components/Menu/Menu.jsx
+++ React/src/components/Menu/Menu.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../share/Nav'
 import Footer from '../share/Footer'
+import Header from '../share/Header'
 export default function Menu() {
   return (
     <div>
       <Nav title='menu'/>
+      <Header title='لذت غذای سالم و گیاهی را با ترخینه تجربه کنید!'/>
       <Footer/>
     </div>
   )

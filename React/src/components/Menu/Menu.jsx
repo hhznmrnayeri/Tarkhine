33ccdd0a3af39e060108e88b32a5150385c83ba5
Heydar <h_h_z_n@yahoo.com>
2024-06-09 09:05:59 +0330

    create Type component and import to Menu.jsx

--- React/src/components/Menu/Menu.jsx
+++ React/src/components/Menu/Menu.jsx
@@ -3,12 +3,22 @@ import Nav from '../share/Nav'
 import Footer from '../share/Footer'
 import Header from '../share/Header'
 import Topic from './Topic'
+import Type from './Type'
+import Search from './Search'
 export default function Menu() {
   return (
     <div>
       <Nav title='menu'/>
       <Header title='لذت غذای سالم و گیاهی را با ترخینه تجربه کنید!'/>
       <Topic/>
+      <section className="mt-2 md:mt-4">
+            <div className="container">
+                <div className="flex flex-col md:flex-row items-start md:items-center gap-3 justify-between">
+                  <Type/>
+                  <Search/>
+                </div>
+            </div>
+      </section>
       <Footer/>
     </div>
   )

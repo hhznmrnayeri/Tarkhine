6b91d813089195f2b51080451e6fa6991fea5128
Heydar <h_h_z_n@yahoo.com>
2024-06-09 07:39:37 +0330

    create Topic component and import to Menu.jsx

--- React/src/components/Menu/Menu.jsx
+++ React/src/components/Menu/Menu.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Nav from '../share/Nav'
 import Footer from '../share/Footer'
 import Header from '../share/Header'
+import Topic from './Topic'
 export default function Menu() {
   return (
     <div>
       <Nav title='menu'/>
       <Header title='لذت غذای سالم و گیاهی را با ترخینه تجربه کنید!'/>
+      <Topic/>
       <Footer/>
     </div>
   )
---
+++ React/src/components/Menu/Topic.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+export default function Topic() {
+    const [topic,setTopic]=useState('غذای اصلی');
+    const changeTopic=(e)=>{
+        setTopic(e.target.innerHTML)
+    }
+return (
+    <div className=" bg-gray-300">
+    <div className="container">
+        <div className="flex items-center gap-4 md:gap-8 text-gray-700 text-xs md:text-xl py-1.5 md:py-3.5">
+            <button onClick={(e)=>changeTopic(e)} className={`${topic==='غذای اصلی'?'topic__item--active':''}`}>غذای اصلی</button>
+            <button onClick={(e)=>changeTopic(e)} className={`${topic==='پیش غذا'?'topic__item--active':''}`}>پیش غذا</button>
+            <button onClick={(e)=>changeTopic(e)} className={`${topic==='دسر'?'topic__item--active':''}`}>دسر</button>
+            <button onClick={(e)=>changeTopic(e)} className={`${topic==='نوشیدنی'?'topic__item--active':''}`}>نوشیدنی</button>
+        </div>
+    </div>
+</div>
+)
+}

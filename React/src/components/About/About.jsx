38b9bd86562657e0b80502231d48cf476a2aaeaf
Heydar <h_h_z_n@yahoo.com>
2024-06-10 10:37:15 +0330

    create Option component and import to About.jsx

--- React/src/components/About/About.jsx
+++ React/src/components/About/About.jsx
@@ -3,12 +3,14 @@ import Nav from '../share/Nav'
 import Header from '../share/Header'
 import Footer from '../share/Footer'
 import Section from './Section'
+import Option from './Option'
 export default function About() {
   return (
     <div>
       <Nav title='about'/>
       <Header title='درباره ترخینه بیشتر بدانید!' background='bg-header-about'/>
       <Section/>
+      <Option/>
       <Footer/>
     </div>
   )

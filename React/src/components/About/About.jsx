98d26d5f86f31f8ca0fc5025459a26805aa61ddf
Heydar <h_h_z_n@yahoo.com>
2024-06-10 10:12:28 +0330

    create Section component and import to About.jsx

--- React/src/components/About/About.jsx
+++ React/src/components/About/About.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Nav from '../share/Nav'
 import Header from '../share/Header'
 import Footer from '../share/Footer'
+import Section from './Section'
 export default function About() {
   return (
     <div>
       <Nav title='about'/>
       <Header title='درباره ترخینه بیشتر بدانید!' background='bg-header-about'/>
+      <Section/>
       <Footer/>
     </div>
   )

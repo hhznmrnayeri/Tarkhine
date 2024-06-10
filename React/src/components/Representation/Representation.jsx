dcd440bc573de81d0757f9f4472e7e1c79c28a72
Heydar <h_h_z_n@yahoo.com>
2024-06-10 07:21:37 +0330

    create Header component and import to Representation.jsx

--- React/src/components/Representation/Representation.jsx
+++ React/src/components/Representation/Representation.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../share/Nav'
 import Footer from '../share/Footer'
+import Header from '../share/Header'
 export default function Representation() {
   return (
     <div>
       <Nav title='representation'/>
+      <Header title='همین الان به خانواده بزرگ ترخینه بپیوندید!' background='bg-header-representation'/>
       <Footer/>
     </div>
   )
---
+++ React/src/components/share/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function Header(props) {
+return (
+<div className={`${props.background} h-44 md:h-80 bg-cover bg-center pt-14 md:pt-32 px-6 relative`}>
+    <div className="flex-center text-center">
+        <h1 className="text-tint-100 font-estedadBold md:text-4xl">{props.title}</h1>
+    </div>
+</div>
+)
+}

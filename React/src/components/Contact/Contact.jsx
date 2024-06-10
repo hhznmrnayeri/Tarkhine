39ed4dab4a7b2bbc2f2462514cd53b4fd28761b9
Heydar <h_h_z_n@yahoo.com>
2024-06-10 11:21:21 +0330

    create Section component and import to Contact.jsx

--- React/src/components/Contact/Contact.jsx
+++ React/src/components/Contact/Contact.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Nav from '../share/Nav'
 import Footer from '../share/Footer'
 import Header from '../share/Header'
+import Section from './Section'
 export default function Contact() {
   return (
     <div>
       <Nav title='contact'/>
       <Header title='با ترخینه در تماس باشید.' background='bg-header-contact'/>
+      <Section/>
       <Footer/>
     </div>
   )

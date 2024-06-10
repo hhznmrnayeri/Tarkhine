7a2c0254174b957d1a651236bf1891172364c6c1
Heydar <h_h_z_n@yahoo.com>
2024-06-10 14:54:43 +0330

    import Outlet to Question.jsx

--- React/src/components/Question/Question.jsx
+++ React/src/components/Question/Question.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Nav from '../share/Nav'
 import Footer from '../share/Footer'
+import { Outlet } from 'react-router-dom'
+// import { useRoutes } from 'react-router-dom'
+// import routes from '../../routes'
 export default function Question() {
+  // const router=useRoutes(routes)
   return (
     <div>
       <Nav title='about'/>
+      {/* {router} */}
+      <Outlet/>
       <Footer/>
     </div>
   )

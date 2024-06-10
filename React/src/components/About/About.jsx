e1844e39fa1f73873033ef17ad1147310a4d8822
Heydar <h_h_z_n@yahoo.com>
2024-06-10 10:09:25 +0330

    import Header component to About.jsx

--- React/src/components/About/About.jsx
+++ React/src/components/About/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../share/Nav'
+import Header from '../share/Header'
 import Footer from '../share/Footer'
 export default function About() {
   return (
     <div>
       <Nav title='about'/>
+      <Header title='درباره ترخینه بیشتر بدانید!' background='bg-header-about'/>
       <Footer/>
     </div>
   )

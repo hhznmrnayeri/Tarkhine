3ce83564d263ea99efba36e80f65abc30920f82a
Heydar <h_h_z_n@yahoo.com>
2024-06-10 07:39:54 +0330

    create Option component and import to Representation.jsx

--- React/src/components/Representation/Option.jsx
+++ React/src/components/Representation/Option.jsx
@@ -8,7 +8,7 @@ import OptionItem from './OptionItem';
 export default function Option() {
 return (
 <div className='container'>
-    <div className="flex-center gap-4 md:gap-12 xl:gap-16 text-xs md:text-lg py-4 md:py-14 flex-wrapborder-b border-gray-400">
+    <div className="flex-center gap-4 md:gap-12 xl:gap-16 text-xs md:text-lg py-4 md:py-14 flex-wrap border-b border-gray-400">
         <OptionItem title='بیش از 20 شعبه فعال در سراسر کشور' icon={<RiBankLine/>}/>
         <OptionItem title='تسهیلات راه‌اندازی رستوران و تجهیز آن' icon={<IoWalletOutline/>}/>
         <OptionItem title='طرح‌های تشویقی ارتقای فروش' icon={<TfiBarChart/>}/>
--- React/src/components/Representation/Representation.jsx
+++ React/src/components/Representation/Representation.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Nav from '../share/Nav'
 import Footer from '../share/Footer'
 import Header from '../share/Header'
+import Option from './Option'
 export default function Representation() {
   return (
     <div>
       <Nav title='representation'/>
       <Header title='همین الان به خانواده بزرگ ترخینه بپیوندید!' background='bg-header-representation'/>
+      <Option/>
       <Footer/>
     </div>
   )

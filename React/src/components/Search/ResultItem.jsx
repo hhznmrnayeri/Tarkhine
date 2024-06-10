ba87411fc111207d26563822aec6278865204df4
Heydar <h_h_z_n@yahoo.com>
2024-06-10 12:02:39 +0330

    create ResultItem component and import to Search.jsx

---
+++ React/src/components/Search/ResultItem.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { IoMdStar } from "react-icons/io";
+import { IoMdHeartEmpty } from "react-icons/io";
+export default function ResultItem(props) {
+return (
+<div className="col-span-1 sm:col-span-6 xl:col-span-3 flex flex-col rounded md:rounded-lg border border-gray-400 overflow-hidden">
+    {/* result img */}
+    <img src={props.img} alt="result" className="result__img object-cover w-full"/>
+    {/* result content */}
+    <div className="bg-white p-4 flex-center flex-col gap-4">
+        {/* result name */}
+        <h2 className="result__name text-xs md:text-xl font-estedadSemiBold">{props.title}</h2>
+        {/* result content wrapper */}
+        <div className="flex items-start justify-between w-full">
+            {/* right box */}
+            <div className="flex flex-col items-start gap-1 text-2xs text-gray-500">
+                {/* top box */}
+                <div className="flex items-center gap-1">
+                    {/* add favorite */}
+                    <button className="add__favorite">
+                        <IoMdHeartEmpty className="w-4 h-4"/>
+                    </button>
+                    {/* text favorite */}
+                    <span>افزودن به علاقمندی‌ها</span>
+                </div>
+                {/* bottom box */}
+                <div className="flex items-center gap-1">
+                    {/* star */}
+                    <IoMdStar className="w-4 h-4 text-[#F4B740]"/>
+                    {/* rate number */}
+                    <span className="rate__number text-2xs md:text-sm text-gray-800 font-estedadMedium">{props.star}</span>
+                    {/* points */}
+                    <span>(<span className="points">{props.points}</span> امتیاز)</span>
+                </div>
+            </div>
+            {/* left box */}
+            <div className="flex flex-col items-end gap-1">
+                {/* top box */}
+                <div className="flex items-center gap-2 text-2xs">
+                    {/* main price */}
+                    <span className="main__price text-gray-500 line-through">{props.price}</span>
+                    {/* count offer */}
+                    <span className={`${props.discount?'count__offer text-error px-1.5 rounded-lg bg-error-200':''}`}>{props.discount?`% ${props.discount}`:''}</span>
+                </div>
+                {/* offer price */}
+                <span className="text-2xs md:text-base"><span className="offer__price">{props.offer} </span>تومان</span>
+            </div>
+        </div>
+        {/* btn result */}
+        <button className="text-2xs md:text-base md:font-estedadMedium text-white px-4 py-2 w-full bg-primary flex-center rounded">افزودن به سبد خرید</button>
+    </div>
+</div>
+)
+}
--- React/src/components/Search/Search.jsx
+++ React/src/components/Search/Search.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from '../share/Nav'
+import ResultItem from './ResultItem'
 import Footer from '../share/Footer'
+import { RiSearchLine } from "react-icons/ri";
 export default function Search() {
+  const [resultArray]=useState([]);
+  // const [resultArray]=useState([{img:['src/assets/images/menu/food32.webp',], title:'کوفته برنجی', resepi:'برنج سبزی کوفته لپه آرد نخودچی، گردو و زرشک و آلو پیاز', offerWrapper:'flex', price:'۱۸۰٬۰۰۰', discount:'۳۵', offer:'۱۴۵٬۰۰۰', comment:'12', star:'4'}]);
+  const [searchValue,setSearchValue]=useState('پاستا')
   return (
     <div>
       <Nav title='search'/>
+      <div className="container mb-6 md:mb-12">
+            {resultArray.length?null:<p className="undefined__caption text-xl text-center mt-12">موردی با این مشخصات پیدا نکردیم!</p>}
+            {resultArray.length?<h1 className="result__title font-estedadBold text-xl text-center">نتایج جستجو برای: <span className="food__result text-primary">{searchValue}</span></h1>:null}
+            <form className="border border-gray-400 rounded-lg mt-6 flex-center px-4 py-2 text-gray-800 text-sm font-estedadMedium w-full xs:w-96 mx-auto">
+                <input type="text" className="w-full h-full outline-none" value={searchValue} onChange={e=>setSearchValue(e.target.value)}/>
+                <button>
+                    <RiSearchLine className="w-6 h-6"/>
+                </button>
+            </form>
+            {resultArray.length?null:<img src="src/assets/images/home/undefined.webp" alt="undefined" className="undefined__img mx-auto mt-14"/>}
+            <div className="result__wrapper grid grid-cols-1 sm:grid-cols-12 gap-6 items-center justify-items-center content-center w-full justify-center mt-12">
+              {resultArray.map((item,index)=>(
+                <ResultItem key={index+1} {...item}/>
+              ))}
+            </div>
+        </div>
       <Footer/>
     </div>
   )

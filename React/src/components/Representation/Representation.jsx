6b35367c0c5429c84e9789006169784e72ed6405
Heydar <h_h_z_n@yahoo.com>
2024-06-10 07:47:05 +0330

    create Advantage component and import to Representation.jsx

--- React/src/components/Representation/Representation.jsx
+++ React/src/components/Representation/Representation.jsx
@@ -3,12 +3,14 @@ import Nav from '../share/Nav'
 import Footer from '../share/Footer'
 import Header from '../share/Header'
 import Option from './Option'
+import Advantage from './Advantage'
 export default function Representation() {
   return (
     <div>
       <Nav title='representation'/>
       <Header title='همین الان به خانواده بزرگ ترخینه بپیوندید!' background='bg-header-representation'/>
       <Option/>
+      <Advantage/>
       <Footer/>
     </div>
   )

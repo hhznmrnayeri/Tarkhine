8bb7ba5aaf58ab0ca79d67e06f6f2977dcfdf959
Heydar <h_h_z_n@yahoo.com>
2024-06-11 07:49:19 +0330

    import Outlet to Buy.jsx

--- React/src/components/Buy/Buy.jsx
+++ React/src/components/Buy/Buy.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../share/Nav'
 import Footer from '../share/Footer'
+import { Outlet } from 'react-router-dom'
 export default function Buy() {
   return (
     <div>
       <Nav title='buy'/>
+      <Outlet/>
       <Footer/>
     </div>
   )

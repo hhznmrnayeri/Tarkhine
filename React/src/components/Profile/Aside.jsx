33c90f4cd35066f05f9f55859820c492c0b465bd
Heydar <h_h_z_n@yahoo.com>
2024-06-13 13:08:52 +0330

    refactor Aside component

--- React/src/components/Profile/Aside.jsx
+++ React/src/components/Profile/Aside.jsx
@@ -13,7 +13,6 @@ import { IoLocationOutline } from "react-icons/io5";
 import { IoLocationSharp } from "react-icons/io5";
 import { TbLogout2 } from "react-icons/tb";
 import { GoChevronRight } from "react-icons/go";
-import Overlay from '../share/Overlay';
 export default function Aside() {
     const [menuItemActive,setMenuItemActive]=useState('user');
     const [showMenu,setShowMenu]=useState(false);
@@ -24,12 +23,11 @@ export default function Aside() {
         setShowMenu(false);
     }
 return (
-<>
 <div className="container">
             {/* section wrapper */}
             <div className="grid grid-cols-1 md:grid-cols-12 gap-6 my-6 md:my-12">
                 {/* state wrapper */}
-                <div className="hidden top-20  bottom-0  fixed md:static bg-white w-full h-full md:h-80 px-5  md:col-span-3 md:flex flex-col md:border border-gray-400 md:px-2 md:py-4 md:rounded-lg transition-all duration-300">
+                <div className={`top-20 bottom-0 ${showMenu?'left-0':'left-full'} z-10 fixed md:static bg-white w-full h-full md:h-80 px-5  md:col-span-3 md:flex flex-col md:border border-gray-400 md:px-2 md:py-4 md:rounded-lg transition-all duration-300`}>
                     {/* top wrapper */}
                     <div className="flex items-center md:items-end gap-2 md:gap-5 pb-2 border-b border-gray-600">
                         {/* img */}
@@ -45,7 +43,9 @@ return (
                     {/* state menu */}
                     <ul className="flex flex-col pr-2 gap-1 text-xs md:text-sm mt-2">
                         {/* state item */}
-                        <button onClick={()=>setMenuItemActive('user')} className={`${menuItemActive==='user'?'profile__state--active':''} flex items-center gap-1 py-2.5 md:py-1.5`}>
+                        <button onClick={()=>{setMenuItemActive('user');
+                            closeAsideMenu()
+                        }} className={`${menuItemActive==='user'?'profile__state--active':''} flex items-center gap-1 py-2.5 md:py-1.5`}>
                             {/* state icon */}
                             <HiOutlineUser className="w-4 h-4 icon__empty"/>
                             {/* state icon */}
@@ -54,7 +54,9 @@ return (
                             <h6>پروفایل</h6>
                         </button>
                         {/* state item */}
-                        <button onClick={()=>setMenuItemActive('wallet')} className={`${menuItemActive==='wallet'?'profile__state--active':''} flex items-center gap-1 py-2.5 md:py-1.5`}>
+                        <button onClick={()=>{setMenuItemActive('wallet');
+                            closeAsideMenu()
+                        }} className={`${menuItemActive==='wallet'?'profile__state--active':''} flex items-center gap-1 py-2.5 md:py-1.5`}>
                             {/* state icon */}
                             <IoWalletOutline className="w-4 h-4 icon__empty"/>
                             {/* state icon */}
@@ -63,7 +65,9 @@ return (
                             <h6>پیگیری سفارشات</h6>
                         </button>
                         {/* state item */}
-                        <button onClick={()=>setMenuItemActive('heart')} className={`${menuItemActive==='heart'?'profile__state--active':''} flex items-center gap-1 py-2.5 md:py-1.5`}>
+                        <button onClick={()=>{setMenuItemActive('heart');
+                            closeAsideMenu()
+                        }} className={`${menuItemActive==='heart'?'profile__state--active':''} flex items-center gap-1 py-2.5 md:py-1.5`}>
                             {/* state icon */}
                             <FaRegHeart className="w-4 h-4 icon__empty"/>
                             {/* state icon */}
@@ -72,7 +76,9 @@ return (
                             <h6>علاقمندی‌ها</h6>
                         </button>
                         {/* state item */}
-                        <button onClick={()=>setMenuItemActive('location')} className={`${menuItemActive==='location'?'profile__state--active':''} flex items-center gap-1 py-2.5 md:py-1.5`}>
+                        <button onClick={()=>{setMenuItemActive('location');
+                            closeAsideMenu()
+                        }} className={`${menuItemActive==='location'?'profile__state--active':''} flex items-center gap-1 py-2.5 md:py-1.5`}>
                             {/* state icon */}
                             <IoLocationOutline className="w-4 h-4 icon__empty"/>
                             {/* state icon */}
@@ -111,77 +117,5 @@ return (
                 </div>
             </div>
         </div>
-{showMenu&&<Overlay onHide={closeAsideMenu} backgroundNone={true}>
-<div className={`${showMenu?'left-0':'left-full'} top-20 bottom-0 fixed bg-white w-full h-full px-5 flex flex-col transition-all duration-300`}>
-    {/* top wrapper */}
-    <div className="flex items-center md:items-end gap-2 md:gap-5 pb-2 border-b border-gray-600">
-        {/* img */}
-        <img src="src/assets/images/profile/Ellipse.webp" alt="user" className="w-12 h-12 lg:w-20 lg:h-20"/>
-        {/* user content */}
-        <div className="flex flex-col items-start">
-            {/* user name */}
-            <h4 className="user__name text-sm lg:text-base">کاربر ترخینه</h4>
-            {/* user phone */}
-            <h5 className="text-gray-700 text-2xs lg:text-xs" dir="ltr">۰۹۱۴ ۸۶۴ ۳۳۵۰</h5>
-        </div>
-    </div>
-    {/* state menu */}
-    <ul className="flex flex-col pr-2 gap-1 text-xs md:text-sm mt-2">
-        {/* state item */}
-        <button onClick={()=>{setMenuItemActive('user');
-            closeAsideMenu()
-        }} className={`${menuItemActive==='user'?'profile__state--active':''} flex items-center gap-1 py-2.5 md:py-1.5`}>
-            {/* state icon */}
-            <HiOutlineUser className="w-4 h-4 icon__empty"/>
-            {/* state icon */}
-            <HiUser className="w-4 h-4 icon__fill"/>
-            {/* state title */}
-            <h6>پروفایل</h6>
-        </button>
-        {/* state item */}
-        <button onClick={()=>{setMenuItemActive('wallet');
-            closeAsideMenu()
-        }} className={`${menuItemActive==='wallet'?'profile__state--active':''} flex items-center gap-1 py-2.5 md:py-1.5`}>
-            {/* state icon */}
-            <IoWalletOutline className="w-4 h-4 icon__empty"/>
-            {/* state icon */}
-            <IoWallet className="w-4 h-4 icon__fill"/>
-            {/* state title */}
-            <h6>پیگیری سفارشات</h6>
-        </button>
-        {/* state item */}
-        <button onClick={()=>{setMenuItemActive('heart');
-            closeAsideMenu()
-        }} className={`${menuItemActive==='heart'?'profile__state--active':''} flex items-center gap-1 py-2.5 md:py-1.5`}>
-            {/* state icon */}
-            <FaRegHeart className="w-4 h-4 icon__empty"/>
-            {/* state icon */}
-            <FaHeart className="w-4 h-4 icon__fill"/>
-            {/* state title */}
-            <h6>علاقمندی‌ها</h6>
-        </button>
-        {/* state item */}
-        <button onClick={()=>{setMenuItemActive('location');
-            closeAsideMenu()
-        }} className={`${menuItemActive==='location'?'profile__state--active':''} flex items-center gap-1 py-2.5 md:py-1.5`}>
-            {/* state icon */}
-            <IoLocationOutline className="w-4 h-4 icon__empty"/>
-            {/* state icon */}
-            <IoLocationSharp className="w-4 h-4 icon__fill"/>
-            {/* state title */}
-            <h6>آدرس‌های من</h6>
-        </button>
-        {/* state item */}
-        <button className="text-error flex items-center gap-1 py-2.5 md:py-1.5">
-            {/* state icon */}
-            <TbLogout2 className="w-4 h-4"/>
-            {/* state title */}
-            <h6>خروج</h6>
-        </button>
-    </ul>
-</div>
-</Overlay>
-}
-</>
 )
 }

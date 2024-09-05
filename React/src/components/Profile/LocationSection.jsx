cdacbb05898e360c866b555b1e488f170e19a338
Heydar <h_h_z_n@yahoo.com>
2024-09-05 15:26:41 +0330

    coding address functions in LocationSection page

--- React/src/components/Profile/LocationSection.jsx
+++ React/src/components/Profile/LocationSection.jsx
@@ -1,30 +1,111 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddressItem from "../share/AddressItem";
 import Empty from "../share/Empty";
 import Overlay from "../share/Overlay";
 import { IoLocationOutline } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
 import { PiGpsFix } from "react-icons/pi";
+import BaseUrl from "../share/BaseUrl";
 export default function LocationSection() {
-  const [addressArray, setAddressArray] = useState([
-    {
-      caption: "تهران: اقدسیه، بزرگراه ارتش، مجتمع شمیران سنتر، طبقه ۱۰",
-      name: "محل کار",
-      user: "سردار وظیفه",
-      phone: "۰۹۱۴ ۸۶۴ ۳۳۵۰",
-      active: true,
-    },
-  ]);
+  const [addressArray, setAddressArray] = useState([]);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [mainId, setMainId] = useState(null);
   const [showLocationModal, setShowLocationModal] = useState(false);
   const [showAddressModal, setShowAddressModal] = useState(false);
   const [nameAddress, setNameAddress] = useState("");
   const [phoneAddress, setPhoneAddress] = useState("");
   const [captionAddress, setCaptionAddress] = useState("");
+  const getAddress = () => {
+    fetch(`${BaseUrl}/address`)
+      .then((res) => res.json())
+      .then((data) => setAddressArray(data));
+  };
+  const openEditModal = (id) => {
+    fetch(`${BaseUrl}/address/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setNameAddress(data.name);
+        setCaptionAddress(data.caption);
+        setPhoneAddress(data.phone);
+        setMainId(data.id);
+        setShowEditModal(true);
+      });
+  };
+  const editAddressItem = () => {
+    const newAddress = {
+      userId: "1",
+      caption: captionAddress,
+      name: nameAddress,
+      phone: phoneAddress,
+      active: true,
+    };
+    fetch(`${BaseUrl}/address/${mainId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newAddress),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        setCaptionAddress("");
+        setNameAddress("");
+        setPhoneAddress("");
+        setMainId(null);
+        getAddress();
+        closeEditModal();
+      });
+  };
+  const activeAddressItem = (id) => {
+    addressArray.forEach((item) => {
+      const newItem = { active: false };
+      fetch(`${BaseUrl}/address/${item.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newItem),
+      })
+        .then((res) => res.json())
+        .then(() => {});
+    });
+    const newItem = { active: true };
+    fetch(`${BaseUrl}/address/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newItem),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        getAddress();
+      });
+  };
+  const removeAddressItem = (id) => {
+    fetch(`${BaseUrl}/address/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => getAddress());
+  };
   const closeLocationModal = () => {
     setShowLocationModal(false);
   };
   const openLocationModal = () => {
     setShowLocationModal(true);
+  };
+  const closeDeleteModal = () => {
+    setShowDeleteModal(false);
+  };
+  const openDeleteModal = () => {
+    setShowDeleteModal(true);
+  };
+  const closeEditModal = () => {
+    setShowEditModal(false);
+    setShowAddressModal(false);
+    setShowLocationModal(false);
   };
   const closeAddressModal = () => {
     setShowAddressModal(false);
@@ -48,13 +129,22 @@ export default function LocationSection() {
     setPhoneAddress("");
     setCaptionAddress("");
   };
+  useEffect(() => {
+    getAddress();
+  }, []);
   return (
     <>
       <section>
         {addressArray.length ? (
           <div className="mt-6 md:mt-4 w-full flex items-start justify-between flex-col xl:flex-row flex-wrap pt-4 gap-2 md:gap-4">
             {addressArray.map((item, index) => (
-              <AddressItem key={index + 1} {...item} />
+              <AddressItem
+                key={index + 1}
+                {...item}
+                onEdit={openEditModal}
+                onRemove={removeAddressItem}
+                onActive={activeAddressItem}
+              />
             ))}
           </div>
         ) : (
@@ -185,6 +275,115 @@ export default function LocationSection() {
                 </button>
               </div>
             </form>
+          </div>
+        </Overlay>
+      )}
+      {showEditModal && (
+        <Overlay onHide={closeEditModal}>
+          <div className="fixed overflow-hidden rounded-lg bg-white w-full flex flex-col md:w-7/12 h-full md:h-5/6 inset-0 m-auto z-30 overflow-y-auto">
+            {/* top wrapper */}
+            <div className="bg-gray-100 flex items-center justify-between py-4 px-6 text-sm font-estedadMedium md:text-2xl md:font-estedadSemiBold">
+              <h3 className="mx-auto">ثبت آدرس</h3>
+              <button onClick={closeEditModal} className="text-gray-700">
+                <IoMdClose className="w-6 h-6" />
+              </button>
+            </div>
+            <img
+              src="../src/assets/images/buy/map.webp"
+              alt="map"
+              className="md:hidden h-60"
+            />
+            {/* form address */}
+            <form
+              className="flex flex-col mt-3 md:mt-6 text-xs md:text-sm px-6"
+              onSubmit={(e) => {
+                e.preventDefault();
+              }}
+            >
+              {/* address name */}
+              <input
+                type="text"
+                className="w-full outline-none border border-gray-400 rounded p-2"
+                placeholder="عنوان آدرس"
+                value={nameAddress}
+                onChange={(e) => setNameAddress(e.target.value)}
+              />
+              {/* checkbox input */}
+              <div className="flex items-center gap-1 mt-3 md:mt-4">
+                <input type="checkbox" name="address" />
+                <label htmlFor="address">تحویل گیرنده خودم هستم.</label>
+              </div>
+              {/* phone */}
+              <input
+                type="text"
+                className="w-full outline-none border border-gray-400 rounded p-2 mt-2"
+                placeholder="شماره همراه"
+                value={phoneAddress}
+                onChange={(e) => setPhoneAddress(e.target.value)}
+              />
+              {/* address text */}
+              <input
+                type="text"
+                className="w-full outline-none border border-gray-400 rounded px-2 mt-3 md:mt-4 pt-2 pb-24 md:pb-44 align-top"
+                placeholder="آدرس دقیق شما"
+                value={captionAddress}
+                onChange={(e) => setCaptionAddress(e.target.value)}
+              />
+              {/* btn wrapper */}
+              <div className="flex items-center gap-6 mt-6 md:mt-4 mb-2">
+                {/* close */}
+                <button
+                  onClick={closeEditModal}
+                  className="text-primary flex-1"
+                >
+                  انصراف
+                </button>
+                {/* submit address */}
+                <button
+                  onClick={editAddressItem}
+                  className="text-white bg-primary flex-1 flex-center rounded p-2"
+                >
+                  ثبت آدرس
+                </button>
+              </div>
+            </form>
+          </div>
+        </Overlay>
+      )}
+      {showDeleteModal && (
+        <Overlay onHide={closeDeleteModal}>
+          <div className="deleteAll__modal fixed overflow-hidden rounded-lg bg-white w-11/12 md:w-5/12 h-48 md:h-56 inset-0 m-auto z-30">
+            {/* top wrapper */}
+            <div className="bg-gray-100 flex items-center justify-between py-4 px-6 text-sm font-estedadMedium md:text-2xl md:font-estedadSemiBold">
+              <h3 className="mx-auto">حذف محصولات</h3>
+              <button
+                onClick={closeDeleteModal}
+                className="close__deleteAll--modal text-gray-700"
+              >
+                <IoMdClose className="w-6 h-6" />
+              </button>
+            </div>
+            {/* caption */}
+            <p className="mt-3 md:mt-8 text-xs md:text-base text-center">
+              همه محصولات سبد خرید شما حذف شود؟
+            </p>
+            {/* btn wrapper */}
+            <div className="mt-8 flex-center gap-4 md:gap-5 px-16">
+              {/* back btn */}
+              <button
+                onClick={closeDeleteModal}
+                className="back__btn border border-primary rounded flex-center p-2 text-primary text-xs md:px-4 md:text-base md:font-estedadMedium flex-1"
+              >
+                بازگشت
+              </button>
+              {/* delete btn */}
+              <button
+                onClick={clearAllBasket}
+                className="delete__btn bg-error-200 rounded flex-center p-2 text-error text-xs md:px-4 md:text-base md:font-estedadMedium flex-1"
+              >
+                حذف
+              </button>
+            </div>
           </div>
         </Overlay>
       )}

aec69fb23ea4faf669793bd2b62469b04cc1b11b
Heydar <h_h_z_n@yahoo.com>
2024-09-30 11:04:46 +0330

    refactor Basket component

--- React/src/components/Buy/Basket.jsx
+++ React/src/components/Buy/Basket.jsx
@@ -1,87 +1,66 @@
 import React, { useEffect, useState } from "react";
-import BaseUrl from "../share/BaseUrl";
 import OrderItem from "./OrderItem";
-import ConvertToPersian from "../../hooks/ConvertToPersian";
+import { useBasket } from "../../context/BasketContext";
+import BaseUrl from "../share/BaseUrl";
+const getBasketArray = () => {
+  const basket = localStorage.getItem("tarkhine-basket");
+  return basket ? JSON.parse(basket) : [];
+};
+const saveBasketArray = (basketArray) => {
+  localStorage.setItem("tarkhine-basket", JSON.stringify(basketArray));
+};
 export default function Basket() {
   const [basketArray, setBasketArray] = useState([]);
-  const [isLogin] = useState(true);
-  const [sumPrices, setSumPrices] = useState(0);
-  const [offPrices, setOffPrices] = useState(0);
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
-  function getBasketArray() {
-    setBasketArray([]);
-    setSumPrices(0);
-    setOffPrices(0);
-    fetch(`${BaseUrl}/basket`)
-      .then((res) => res.json())
-      .then((data) => {
-        data.forEach((item) => {
-          fetch(`${BaseUrl}/foods/${item.foodId}`)
-            .then((res) => res.json())
-            .then((data) => {
-              setSumPrices((prev) => prev + item.count * data.priceValue);
-              if (data.offerPrice) {
-                setOffPrices(
-                  (prev) =>
-                    prev + item.count * (data.offerPrice - data.priceValue)
-                );
-              }
-              setBasketArray((prev) => [...prev, { ...data, ...item }]);
-            });
-        });
-      });
-  }
+  const { dispatch } = useBasket();
+
+  const foodItemCreator = async (foodArray) => {
+    const foodPromises = foodArray.map(async (item) => {
+      const foodResponse = await fetch(`${BaseUrl}/foods/${item.foodId}`);
+      const foodData = await foodResponse.json();
+      return { ...foodData, ...item };
+    });
+    const foodDataArray = await Promise.all(foodPromises);
+    dispatch({ type: "updateBasket", payload: foodDataArray });
+    setBasketArray(foodDataArray);
+  };
+  const updateCount = (id, newCount) => {
+    const updatedBasket = basketArray.map((item) =>
+      item.id === id ? { ...item, count: newCount } : item
+    );
+    saveBasketArray(updatedBasket);
+    foodItemCreator(updatedBasket);
+  };
   const increaseCount = (id, counter) => {
-    const newCount = { count: counter + 1 };
-    fetch(`${BaseUrl}/basket/${id}`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newCount),
-    })
-      .then((res) => res.json())
-      .then(() => getBasketArray());
+    updateCount(id, counter + 1);
   };
   const decreaseCount = (id, counter) => {
-    const newCount = { count: counter - 1 };
-    fetch(`${BaseUrl}/basket/${id}`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newCount),
-    })
-      .then((res) => res.json())
-      .then(() => getBasketArray());
+    if (counter > 1) {
+      updateCount(id, counter - 1);
+    }
   };
   const removeItemFromBasket = (id) => {
-    fetch(`${BaseUrl}/basket/${id}`, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then(() => getBasketArray());
+    const updatedBasket = basketArray.filter((item) => item.id !== id);
+    saveBasketArray(updatedBasket);
+    foodItemCreator(updatedBasket);
   };
   useEffect(() => {
-    getBasketArray();
+    const initialBasket = getBasketArray();
+    foodItemCreator(initialBasket);
   }, []);
   return (
-    <>
-      <div
-        dir="ltr"
-        className="col-span-1 lg:col-span-7 h-44 lg:h-[554px] overflow-y-auto flex flex-col md:p-6 md:gap-4 md:border md:border-gray-400 md:rounded-lg overflow-hidden"
-      >
-        {basketArray.map((item) => (
-          <OrderItem
-            key={item.id}
-            {...item}
-            onIncrease={increaseCount}
-            onDecrease={decreaseCount}
-            onRemove={removeItemFromBasket}
-          />
-        ))}
-      </div>
-      {/* <Empty
-              caption="شما در حال حاضر هیچ سفارشی ثبت نکرده‌اید!"
-              href="/menu"
-              btn="منوی رستوران"
-            /> */}
-    </>
+    <div
+      dir="ltr"
+      className="col-span-1 lg:col-span-7 h-44 lg:h-[554px] overflow-y-auto flex flex-col md:p-6 md:gap-4 md:border md:border-gray-400 md:rounded-lg overflow-hidden"
+    >
+      {basketArray.map((item) => (
+        <OrderItem
+          key={item.id}
+          {...item}
+          onIncrease={increaseCount}
+          onDecrease={decreaseCount}
+          onRemove={removeItemFromBasket}
+        />
+      ))}
+    </div>
   );
 }
